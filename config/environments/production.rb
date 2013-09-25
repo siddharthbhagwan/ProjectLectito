8c925b96d0ac13443ca72effb67c8213b263911e
Siddharth Bhagwan <sid_sss@hotmail.com>
2013-09-25 19:51:21 +0530

    Sendgrid prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-ProjectLectito::Application.configure do
+  ProjectLectito::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   config.eager_load = true
