3af0ed824dc73811d739af93ab7189c9a142cf16
Siddharth Bhagwan <sid_sss@hotmail.com>
2013-10-12 16:23:11 +0530

    Site wide chat enabled

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,5 +1,5 @@
 class ChatController < ApplicationController
-  include ActionController::Live
+  include ApplicationHelper
 
   def show
     @transaction = Transaction.where(:id => params[:id]).take
@@ -31,20 +31,6 @@ class ChatController < ApplicationController
   end
   
   def index
-  	user_accepted_transactions =  Transaction.where("((borrower_id = ? OR lender_id = ? ) AND status = ? )", current_user.id , current_user.id, "Accepted")
-    @current_transactions = Array.new
-    @current_transactions_id = Array.new
-  	user_accepted_transactions.each do |t|
-  		if !(User.where(:id => t.borrower_id).take.is_delivery) || !(User.where(:id => t.lender_id).take.is_delivery)
-  			@current_transactions << t
-        @current_transactions_id << t.id
-
-        if current_user.id == t.lender_id
-          @lender_borrower_name = User.find(t.borrower_id).full_name
-        else
-          @lender_borrower_name = User.find(t.lender_id).full_name
-        end
-  	  end
-    end
+  	chatbox()
   end
 end  
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,22 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def chatbox
+    user_accepted_transactions =  Transaction.where("((borrower_id = ? OR lender_id = ? ) AND status = ? )", current_user.id , current_user.id, "Accepted")
+    @current_transactions = Array.new
+    @current_transactions_id = Array.new
+    user_accepted_transactions.each do |t|
+      if !(User.where(:id => t.borrower_id).take.is_delivery) || !(User.where(:id => t.lender_id).take.is_delivery)
+        @current_transactions << t
+        @current_transactions_id << t.id
+
+        if current_user.id == t.lender_id
+          @lender_borrower_name = User.find(t.borrower_id).full_name
+        else
+          @lender_borrower_name = User.find(t.lender_id).full_name
+        end
+      end
+    end
+  end
 end
