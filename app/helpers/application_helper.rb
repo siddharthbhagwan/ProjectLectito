07eaf0856b64e563c3a2f5ab91faa750c2aabb79
Siddharth Bhagwan <sid_sss@hotmail.com>
2013-12-11 11:05:19 +0530

    Current transactions lender borrower name bug fixed. logs removed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,16 +22,6 @@ module ApplicationHelper
     user_accepted_transactions.each do |t|
       if !(User.where(:id => t.borrower_id).take.is_delivery) || !(User.where(:id => t.lender_id).take.is_delivery)
         @current_transactions << t
-        puts "Checking for " + t.id.to_s
-        if current_user.id == t.lender_id
-          @lender_borrower_name = User.find(t.borrower_id).full_name
-          puts " Borrower " + User.find(t.borrower_id).full_name.to_s
-          puts "Lender Borrower " + @lender_borrower_name.to_s
-        else
-          @lender_borrower_name = User.find(t.lender_id).full_name
-          puts " Lender " + User.find(t.lender_id).full_name.to_s
-          puts " Lender Borrower " + @lender_borrower_name.to_s
-        end
       end
     end
   end
