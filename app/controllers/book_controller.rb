be7849281b0ae46f88b56ef4456104c1147e675f
Siddharth Bhagwan <sid_sss@hotmail.com>
2013-12-27 19:04:55 +0530

    Rubocopped Book Controller

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,55 +1,64 @@
+# Controller for Book Model
 class BookController < ApplicationController
-	include ApplicationHelper
-	load_and_authorize_resource :class => Book
+  include ApplicationHelper
+  load_and_authorize_resource class: :Book
 
-	def new
-		@book = Book.new
-		chatbox()
-	end
+  def new
+    @book = Book.new
+    chatbox
+  end
 
-	def create
-		@book = Book.new(params[:book])
-		if @book.save
-			redirect_to book_index_path
-		else
-			render 'new'
-		end
-	end
+  def create
+    @book = Book.new(params[:book])
+    if @book.save
+      redirect_to book_index_path
+    else
+      render 'new'
+    end
+  end
 
-	def index
-		@book = Book.all
-		chatbox()
-	end
+  def index
+    @book = Book.all
+    chatbox
+  end
 
-	def edit
-		@book = Book.find(params[:id])
-		chatbox()
-	end
+  def edit
+    @book = Book.find(params[:id])
+    chatbox
+  end
 
-	def destroy
-		@book = Book.find(params[:id])
-		@book.destroy
-		redirect_to book_index_path
-		flash[:info] = "The Book has been deleted"
-	end
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to book_index_path
+    flash[:info] = 'The Book has been deleted'
+  end
 
-	def update
-		@book = Book.find(params[:id])
-		@book.update_attributes(params[:book])
-		redirect_to book_index_path
-	end
+  def update
+    @book = Book.find(params[:id])
+    @book.update_attributes(params[:book])
+    redirect_to book_index_path
+  end
 
-	def book_status
-		@available = Inventory.where(:deleted => false, :status => "Available", :book_id => params[:book_id]).count
-		@borrowed = Inventory.where(:deleted => false, :status => "Unavailable", :book_id => params[:book_id]).count
-	end
+  def book_status
+    @available = Inventory.where(deleted: :false,
+                                 status: 'Available',
+                                 book_id: params[:book_id]).count
 
-	def available_book_stats
-		@available_list = Inventory.where(:deleted => false, :status => "Available", :book_id => params[:book_id])
-	end
+    @borrowed = Inventory.where(deleted: :false,
+                                status: 'Unavailable',
+                                book_id: params[:book_id]).count
+  end
 
-	def borrowed_book_stats
-		@borrowed_list = Inventory.where(:deleted => false, :status => "Unavailable", :book_id => params[:book_id])
-	end
+  def available_book_stats
+    @available_list = Inventory.where(deleted: :false,
+                                      status: 'Available',
+                                      book_id: params[:book_id])
+  end
 
+  def borrowed_book_stats
+    @borrowed_list = Inventory.where(deleted: :false,
+                                     status: 'Unavailable',
+                                     book_id: params[:book_id])
+  end
 end
