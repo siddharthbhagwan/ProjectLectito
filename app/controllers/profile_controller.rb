0293f63d75c785a73b18e419b981592a148b2286
Siddharth Bhagwan <sid_sss@hotmail.com>
2013-12-27 19:35:14 +0530

    Profile Controller Rubocopped

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,7 +1,8 @@
+# Profile Controller
 class ProfileController < ApplicationController
   include ApplicationHelper
-  Firebase.base_uri = "https://projectlectito.Firebaseio.com/"
-  load_and_authorize_resource :class => Profile
+  Firebase.base_uri = 'https://projectlectito.Firebaseio.com/'
+  load_and_authorize_resource class: :Profile
 
   def new
     @profile = Profile.new
@@ -10,136 +11,134 @@ class ProfileController < ApplicationController
   def create
     @profile = Profile.new(params[:profile])
     @profile.user_id = current_user.id
-    @profile.profile_status = "Online"
-    @profile.delivery = "true"
+    @profile.profile_status = 'Online'
+    @profile.delivery = 'true'
     if @profile.save
-      flash[:notice] = "Your Profile has been created"
+      flash[:notice] = 'Your Profile has been created'
       redirect_to new_address_path
     else
-      redirect_to new_profile_path  
+      redirect_to new_profile_path
     end
   end
 
   def update
-    @profile = Profile.where(:user_id => current_user.id).take
-      if @profile.update_attributes(params[:profile])
-         redirect_to edit_profile_path
-         flash[:notice] = "Your Profile has been updated"
-       else
-        render 'edit'
-      end
+    @profile = Profile.where(user_id: :current_user.id).take
+    if @profile.update_attributes(params[:profile])
+      redirect_to edit_profile_path
+      flash[:notice] = 'Your Profile has been updated'
+     else
+      render 'edit'
+    end
   end
 
   def edit
-    @profile = Profile.where(:id => params[:id]).take
-    chatbox()
+    @profile = Profile.where(id: :params[:id]).take
+    chatbox
   end
 
   def rating
-
     @name = User.find(current_user.id).full_name
 
-    @good_lender = Transaction.where(:lender_id => current_user.id, :borrower_feedback => 'good').count
-    @good_borrower = Transaction.where(:borrower_id => current_user.id, :lender_feedback => 'good').count
+    @good_lender = Transaction.where(lender_id: :current_user.id, borrower_feedback: :good).count
+    @good_borrower = Transaction.where(borrower_id: :current_user.id, lender_feedback: :good).count
     @good = @good_borrower + @good_lender
 
-    @bad_lender = Transaction.where(:lender_id => current_user.id, :borrower_feedback => 'bad').count
-    @bad_borrower = Transaction.where(:borrower_id => current_user.id, :lender_feedback => 'bad').count 
+    @bad_lender = Transaction.where(lender_id: :current_user.id, borrower_feedback: :bad).count
+    @bad_borrower = Transaction.where(borrower_id: :current_user.id, lender_feedback: :bad).count
     @bad = @bad_lender + @bad_borrower
 
-    @neutral_lender = Transaction.where(:lender_id => current_user.id, :borrower_feedback => 'neutral').count
-    @neutral_borrower = Transaction.where(:borrower_id => current_user.id, :lender_feedback => 'neutral').count
+    @neutral_lender = Transaction.where(lender_id: :current_user.id, borrower_feedback: :neutral).count
+    @neutral_borrower = Transaction.where(borrower_id: :current_user.id, lender_feedback: :neutral).count
     @neutral = @neutral_lender + @neutral_borrower
 
-    @total_books = Inventory.where(:user_id => current_user.id, :deleted => :false).count
-    @transactions = Transaction.where("(lender_id = ? OR borrower_id = ?) AND (status != ? OR status != ?)", current_user.id, current_user.id, 'Rejected', 'Cancelled' ).order("created_at desc")
+    @total_books = Inventory.where(user_id: :current_user.id, deleted: :false).count
+    @transactions = Transaction.where('(lender_id = ? OR borrower_id = ?) AND (status != ? OR status != ?)', current_user.id, current_user.id, 'Rejected', 'Cancelled').order('created_at desc')
 
     @total_transactions = @transactions.count
     @comment_history = []
 
     @transactions.each do |t|
       if t.lender_id == current_user.id
-        if t.borrower_comments != "" and !t.borrower_comments.nil?
-          @comment_history.push(t.borrower_comments + " ~ " + User.find(t.borrower_id).full_name)
+        if t.borrower_comments != '' && !t.borrower_comments.nil?
+          @comment_history.push(t.borrower_comments + ' ~ ' + User.find(t.borrower_id).full_name)
         end
       else
-        if t.lender_comments != "" and !t.lender_comments.nil?
-          @comment_history.push(t.lender_comments + " ~ " + User.find(t.lender_id).full_name)
+        if t.lender_comments != '' && !t.lender_comments.nil?
+          @comment_history.push(t.lender_comments + ' ~ ' + User.find(t.lender_id).full_name)
         end
       end
     end
-
   end
 
   def public_rating
     pr = Transaction.find(params[:tr_id])
     id = current_user.id
 
-    if pr.lender_id == current_user.id
+    if pr.lender_id == id
       @name = User.find(pr.borrower_id).full_name
 
-      @good_lender = Transaction.where(:lender_id => pr.borrower_id, :borrower_feedback => 'good').count
-      @good_borrower = Transaction.where(:borrower_id => pr.borrower_id, :lender_feedback => 'good').count
+      @good_lender = Transaction.where(lender_id: :pr.borrower_id, borrower_feedback: :good).count
+      @good_borrower = Transaction.where(borrower_id: :pr.borrower_id, lender_feedback: :good).count
       @good = @good_borrower + @good_lender
 
-      @bad_lender = Transaction.where(:lender_id => pr.borrower_id, :borrower_feedback => 'bad').count
-      @bad_borrower = Transaction.where(:borrower_id => pr.borrower_id, :lender_feedback => 'bad').count 
+      @bad_lender = Transaction.where(lender_id: :pr.borrower_id, borrower_feedback: :bad).count
+      @bad_borrower = Transaction.where(borrower_id: :pr.borrower_id, lender_feedback: :bad).count
       @bad = @bad_lender + @bad_borrower
 
-      @neutral_lender = Transaction.where(:lender_id => pr.borrower_id, :borrower_feedback => 'neutral').count
-      @neutral_borrower = Transaction.where(:borrower_id => pr.borrower_id, :lender_feedback => 'neutral').count
+      @neutral_lender = Transaction.where(lender_id: :pr.borrower_id, borrower_feedback: :neutral).count
+      @neutral_borrower = Transaction.where(borrower_id: :pr.borrower_id, lender_feedback: :neutral).count
       @neutral = @neutral_lender + @neutral_borrower
 
-      @total_books = Inventory.where(:user_id => pr.borrower_id, :deleted => :false).count
-      @transactions = Transaction.where("(lender_id = ? OR borrower_id = ?) AND (status != ? OR status != ?)", pr.borrower_id, pr.borrower_id, 'Rejected', 'Cancelled' ).order("created_at desc")
+      @total_books = Inventory.where(user_id: :pr.borrower_id, deleted: :false).count
+      @transactions = Transaction.where('(lender_id = ? OR borrower_id = ?) AND (status != ? OR status != ?)', pr.borrower_id, pr.borrower_id, 'Rejected', 'Cancelled').order('created_at desc')
 
       @total_transactions = @transactions.count
       @comment_history = []
 
       @transactions.each do |t|
         if t.lender_id == pr.borrower_id
-          if t.borrower_comments != "" and !t.borrower_comments.nil?
-            @comment_history.push(t.borrower_comments + " ~ " + User.find(t.borrower_id).full_name)
+          if t.borrower_comments != '' && !t.borrower_comments.nil?
+            @comment_history.push(t.borrower_comments + ' ~ ' + User.find(t.borrower_id).full_name)
           end
         else
-          if t.lender_comments != "" and !t.lender_comments.nil?
-            @comment_history.push(t.lender_comments + " ~ " + User.find(t.lender_id).full_name)
+          if t.lender_comments != '' && !t.lender_comments.nil?
+            @comment_history.push(t.lender_comments + ' ~ ' + User.find(t.lender_id).full_name)
           end
         end
       end
 
       respond_to do |format|
         format.html { render :rating }
-        format.json { 
+        format.json {
           json_profile = Array.new
           json_profile << {
-            :name => @name,
-            :good => @good,
-            :neutral => @neutral,
-            :bad => @bad,
-            :transactions => @total_transactions,
-            :books => @total_books
+            name: :@name,
+            good: :@good,
+            neutral: :@neutral,
+            bad: :@bad,
+            transactions: :@total_transactions,
+            books: :@total_books
           }
-          render :json => json_profile.to_json }
+          render json: :json_profile.to_json }
       end
 
     elsif  pr.borrower_id == current_user.id
       @name = User.find(pr.lender_id).full_name
 
-      @good_lender = Transaction.where(:lender_id => pr.lender_id, :borrower_feedback => 'good').count
-      @good_borrower = Transaction.where(:borrower_id => pr.lender_id, :lender_feedback => 'good').count 
+      @good_lender = Transaction.where(lender_id: :pr.lender_id, borrower_feedback: :good).count
+      @good_borrower = Transaction.where(borrower_id: :pr.lender_id, lender_feedback: :good).count
       @good = @good_borrower + @good_lender
 
-      @bad_lender = Transaction.where(:lender_id => pr.lender_id, :borrower_feedback => 'bad').count
-      @bad_borrower = Transaction.where(:borrower_id => pr.lender_id, :lender_feedback => 'bad').count 
+      @bad_lender = Transaction.where(lender_id: :pr.lender_id, borrower_feedback: :bad).count
+      @bad_borrower = Transaction.where(borrower_id: :pr.lender_id, lender_feedback: :bad).count
       @bad = @bad_lender + @bad_borrower
 
-      @neutral_lender = Transaction.where(:lender_id => pr.lender_id, :borrower_feedback => 'neutral').count
-      @neutral_borrower = Transaction.where(:borrower_id => pr.lender_id, :lender_feedback => 'neutral').count
+      @neutral_lender = Transaction.where(lender_id: :pr.lender_id, borrower_feedback: :neutral).count
+      @neutral_borrower = Transaction.where(borrower_id: :pr.lender_id, lender_feedback: :neutral).count
       @neutral = @neutral_lender + @neutral_borrower
 
-      @total_books = Inventory.where(:user_id => pr.lender_id, :deleted => :false).count
-      @transactions = Transaction.where("(lender_id = ? OR borrower_id = ?) AND (status != ? OR status != ?)", pr.lender_id, pr.lender_id, 'Rejected', 'Cancelled' ).order("created_at desc")
+      @total_books = Inventory.where(user_id: :pr.lender_id, deleted: :false).count
+      @transactions = Transaction.where('(lender_id = ? OR borrower_id = ?) AND (status != ? OR status != ?)', pr.lender_id, pr.lender_id, 'Rejected', 'Cancelled').order('created_at desc')
 
       @total_transactions = @transactions.count
 
@@ -147,34 +146,34 @@ class ProfileController < ApplicationController
 
       @transactions.each do |t|
         if t.lender_id == pr.lender_id
-          if t.borrower_comments != "" and !t.borrower_comments.nil?
-            @comment_history.push(t.borrower_comments + " ~ " + User.find(t.borrower_id).full_name)
+          if t.borrower_comments != '' && !t.borrower_comments.nil?
+            @comment_history.push(t.borrower_comments + ' ~ ' + User.find(t.borrower_id).full_name)
           end
         else
-          if t.lender_comments != "" and !t.lender_comments.nil?
-            @comment_history.push(t.lender_comments + " ~ " + User.find(t.lender_id).full_name)
+          if t.lender_comments != '' && !t.lender_comments.nil?
+            @comment_history.push(t.lender_comments + ' ~ ' + User.find(t.lender_id).full_name)
           end
         end
       end
 
       respond_to do |format|
         format.html  { render :rating }
-        format.json { 
+        format.json {
           json_profile = Array.new
           json_profile << {
-            :name => @name,
-            :good => @good,
-            :neutral => @neutral,
-            :bad => @bad,
-            :transactions => @total_transactions,
-            :books => @total_books
+            name: :@name,
+            good: :@good,
+            neutral: :@neutral,
+            bad: :@bad,
+            transactions: :@total_transactions,
+            books: :@total_books
           }
-          render :json => json_profile.to_json }
+          render json: :json_profile.to_json }
       end
 
     else
       redirect_to home_path
-      flash[:alert] = "No such page exists!"
+      flash[:alert] = 'No such page exists!'
     end
   end
 
@@ -189,16 +188,16 @@ class ProfileController < ApplicationController
 
     # For Transaction history, only completed transactions need to be listed
     # Fn returns id's (lender or borrower) , and not transaction ids. Thus duplication check greatly helps
-    if params[:page] == "/transaction/history"
-      all_transactions =  Transaction.where("((borrower_id = ? OR lender_id = ? ) AND status = ? )", current_user.id , current_user.id, "Complete").order("request_date desc")
+    if params[:page] == '/transaction/history'
+      all_transactions =  Transaction.where('((borrower_id = ? OR lender_id = ? ) AND status = ? )', current_user.id , current_user.id, 'Complete').order('request_date desc')
       all_transactions.each do |at|
         # if current user is the lender
         if at.lender_id == current_user.id
           # check for duplication
           if !active_trans_ids.include? at.borrower_id
             lsa = User.find(at.borrower_id).profile.last_seen_at
-            # puts " Difference is " + (DateTime.now.to_time - lsa).seconds.to_s
-            if !lsa.nil? and (DateTime.now.to_time - lsa).seconds < 6
+            # puts ' Difference is ' + (DateTime.now.to_time - lsa).seconds.to_s
+            if !lsa.nil? && (DateTime.now.to_time - lsa).seconds < 6
               active_trans_ids.push(at.borrower_id)
             end
           end
@@ -207,8 +206,8 @@ class ProfileController < ApplicationController
           # check for duplication
           if !active_trans_ids.include? at.lender_id
             lsa = User.find(at.lender_id).profile.last_seen_at.to_time
-            #puts " Difference is " + (DateTime.now.to_time - lsa).seconds.to_s
-            if !lsa.nil? and (DateTime.now.to_time - lsa).seconds < 6
+            # puts ' Difference is ' + (DateTime.now.to_time - lsa).seconds.to_s
+            if !lsa.nil? && (DateTime.now.to_time - lsa).seconds < 6
               active_trans_ids.push(at.lender_id)
             end
           end
@@ -216,23 +215,23 @@ class ProfileController < ApplicationController
       end
       # For home page / search page, transaction in progress need to be listed
       # Returns transaction ids. So need need for duplication checks
-    else  
-      # puts " Checking for " + User.find(current_user.id).full_name + " - " + current_user.id.to_s
+    else
+      # puts ' Checking for ' + User.find(current_user.id).full_name + ' - ' + current_user.id.to_s
       #TODO Check if chatbox call is cheaper than the quesries themselves
-      chatbox()
+      chatbox
       @current_transactions.each do |ct|
         # current user is the lender
         if ct.lender_id == current_user.id
           lsa = User.find(ct.borrower_id).profile.last_seen_at
-          # puts " Difference is " + (DateTime.now.to_time - lsa).seconds.to_s
-          if !lsa.nil? and (DateTime.now.to_time - lsa).seconds < 6
+          # puts ' Difference is ' + (DateTime.now.to_time - lsa).seconds.to_s
+          if !lsa.nil? && (DateTime.now.to_time - lsa).seconds < 6
             active_trans_ids.push(ct.id)
           end
         else
           # current user is the borrower
           lsa = User.find(ct.lender_id).profile.last_seen_at.to_time
-          # puts " Difference is " + (DateTime.now.to_time - lsa).seconds.to_s
-          if !lsa.nil? and (DateTime.now.to_time - lsa).seconds < 6
+          # puts ' Difference is ' + (DateTime.now.to_time - lsa).seconds.to_s
+          if !lsa.nil? && (DateTime.now.to_time - lsa).seconds < 6
             active_trans_ids.push(ct.id)
           end
         end
@@ -240,7 +239,7 @@ class ProfileController < ApplicationController
     end
 
     respond_to do |format|
-      format.json { render :json => active_trans_ids.to_json }
+      format.json { render json: :active_trans_ids.to_json }
     end
   end
 end
