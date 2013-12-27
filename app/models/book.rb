aca2de77a722a1bcf583f836b25df757d80f6da6
Siddharth Bhagwan <sid_sss@hotmail.com>
2013-12-27 18:32:31 +0530

    Rubocopped Book Model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,13 +1,11 @@
+# Model describing attributes of a Book
 class Book < ActiveRecord::Base
-  attr_accessible :isbn, :author, :book_name, :edition, :genre, :language, :mrp, :pages, :publisher, :version
+  attr_accessible :isbn, :author, :book_name, :edition, :genre, :language,
+                  :mrp, :pages, :publisher, :version
 
-  #validates :ISBN, :author,:book_name, :presence => {:message => "Cant be empty"}
+  # validates :ISBN,:author,:book_name,presence: {:message => "Cant be empty"}
 
-  #Associations
+  # Associations
   has_many :inventories
-  has_many :users, :through => :inventories
-
-
+  has_many :users, through: :inventories
 end
-
-
