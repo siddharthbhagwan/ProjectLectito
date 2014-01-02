18d08394ebd5519b95e998503b6555550a3e4929
Siddharth Bhagwan <sid_sss@hotmail.com>
2014-01-02 12:45:36 +0530

    Application Controller Rubocopped

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,21 @@
+# Application Controller
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_action :authenticate_user!, :except => [:search_books, :search_books_city, :search, :autocomplete_book_name, :autocomplete_author]
-  before_action :is_user_barred, :except => [:barred, :destroy, :search, :sub_search]
+  before_action :authenticate_user!, except: [:search_books, :search_books_city, :search, :autocomplete_book_name, :autocomplete_author]
+  before_action :user_barred?, except: [:barred, :destroy, :search, :sub_search]
   after_action :update_timestamp
 
   rescue_from CanCan::AccessDenied do |exception|
-    Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
-    redirect_to root_path, :alert => exception.message
+    Rails.logger.debug 'Access denied on #{exception.action} #{exception.subject.inspect}'
+    redirect_to root_path, alert: exception.message
   end
 
-  def is_user_barred
-    if user_signed_in? 
-      if !current_user.profile.nil?
-        if current_user.current_status == "Locked"
-           redirect_to profile_barred_path
-           flash[:alert] = " Account Locked "
+  def user_barred?
+    if user_signed_in?
+      unless current_user.profile.nil?
+        if current_user.current_status == 'Locked'
+          redirect_to profile_barred_path
+          flash[:alert] = ' Account Locked '
         end
       end
     end
