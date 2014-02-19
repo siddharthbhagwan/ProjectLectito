71b6357fa4563cd96c74c617120fe5a33fc26abc
Siddharth Bhagwan <sid_sss@hotmail.com>
2014-02-19 19:09:12 +0530

    Prod Sendgrid changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,10 +77,11 @@
 
   ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
-  :port           => 587,
+  :port           => '587',
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com'
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
 }
 end
