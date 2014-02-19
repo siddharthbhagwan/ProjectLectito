11dc16cb17fc15689c7f1224a66e8d3f6ce8d7d7
Siddharth Bhagwan <sid_sss@hotmail.com>
2014-02-19 18:51:10 +0530

    Prod Sendrig changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,11 +75,11 @@
 
   ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
-  :port           => '587',
+  :port           => '25',
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
+  :domain         => ENV['SENDGRID_DOMAIN'],
   :enable_starttls_auto => true
 }
 end
