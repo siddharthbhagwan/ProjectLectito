2175181c673c6c3e757c4b14d0b591a810a6521f
Siddharth Bhagwan <sid_sss@hotmail.com>
2014-02-20 16:12:10 +0530

    Google Login added

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -15,5 +15,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   alias_method :facebook, :all
   alias_method :twitter, :all
+  alias_method :google_oauth2, :all
 
 end
