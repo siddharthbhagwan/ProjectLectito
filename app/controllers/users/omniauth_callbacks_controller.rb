1896be7ad51f91da88d613c0a5bb5889847de7a3
Siddharth Bhagwan <sid_sss@hotmail.com>
2014-02-20 16:14:34 +0530

    Google Login added

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,13 +2,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def all
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    user=User.from_omniauth(request.env["omniauth.auth"])
+    user=User.from_omniauth(request.env['omniauth.auth'])
 
     if user.persisted?
-      flash.notice = "Signed In!"
+      flash.notice = 'Signed In!'
       sign_in_and_redirect user
     else
-      session["devise.user_attributes"] = user.attributes
+      session['devise.user_attributes'] = user.attributes
       redirect_to new_user_registration_url
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :token_authenticatable, :confirmable,
-  		 :lockable, :timeoutable, :omniauthable, omniauth_providers: [:facebook, :twitter]
+  		 :lockable, :timeoutable, :omniauthable, omniauth_providers: [:facebook, :twitter, :google]
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :roles_mask
