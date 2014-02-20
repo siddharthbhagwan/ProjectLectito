5d8a3e61d24dffb87ef863cbc38fe97b732f0ded
Siddharth Bhagwan <sid_sss@hotmail.com>
2014-02-20 16:08:31 +0530

    Twitter Login added

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,13 +2,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def all
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+    user=User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: 'Facebook' || 'Twitter') if is_navigational_format?
+    if user.persisted?
+      flash.notice = "Signed In!"
+      sign_in_and_redirect user
     else
-      session['devise.facebook_data'] = request.env['omniauth.auth']
+      session["devise.user_attributes"] = user.attributes
       redirect_to new_user_registration_url
     end
   end
