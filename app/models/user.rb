f8279672bed2cc4a27acad06eed98f17afff208b
Siddharth Bhagwan <sid_sss@hotmail.com>
2014-02-20 16:04:30 +0530

    Twitter Login added

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :token_authenticatable, :confirmable,
-  		 :lockable, :timeoutable, :omniauthable, omniauth_providers: [:facebook]
+  		 :lockable, :timeoutable, :omniauthable, omniauth_providers: [:facebook, :twitter]
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :roles_mask
