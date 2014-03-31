19d30d6cc7ec65005820fb0fd80288979e3a692a
Siddharth Bhagwan <sid_sss@hotmail.com>
2014-03-31 18:32:52 +0530

    Profile Blank check added

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -396,78 +396,80 @@ class ProfileController < ApplicationController
   def online
     # Called every n seconds to update timestamp
     online = User.find(current_user.id).profile
-    time_now = DateTime.now
-    online.last_seen_at = time_now
-    # p 'Updating ' + User.find(current_user.id).full_name.upcase + ' to ' + time_now.to_time.to_s
-    online.save
+    unless online.blank?
+      time_now = DateTime.now
+      online.last_seen_at = time_now
+      # p 'Updating ' + User.find(current_user.id).full_name.upcase + ' to ' + time_now.to_time.to_s
+      online.save
 
-    # Array to store the id's whose online status needs to be visible to current user
-    active_trans_ids = Array.new
+      # Array to store the id's whose online status needs to be visible to current user
+      active_trans_ids = Array.new
 
-    # For Transaction history, only completed transactions need to be listed
-    # Fn returns id's (lender or borrower) , and not transaction ids. Thus duplication check greatly helps
-    if params[:page] == '/transaction/history'
-      all_transactions =  Transaction.where('((borrower_id = ? OR lender_id = ? ) AND status = ? )', current_user.id , current_user.id, 'Complete').order('request_date desc')
-      all_transactions.each do |at|
-        # if current user is the lender
-        time_now_update = Time.now
-        if at.lender_id == current_user.id
-          # check for duplication
-          if !active_trans_ids.include? at.borrower_id
-            lsa = User.find(at.borrower_id).profile.last_seen_at
-            # p 'Last seen ' + User.find(at.borrower_id).full_name + ' at ' + lsa.to_s
+      # For Transaction history, only completed transactions need to be listed
+      # Fn returns id's (lender or borrower) , and not transaction ids. Thus duplication check greatly helps
+      if params[:page] == '/transaction/history'
+        all_transactions =  Transaction.where('((borrower_id = ? OR lender_id = ? ) AND status = ? )', current_user.id , current_user.id, 'Complete').order('request_date desc')
+        all_transactions.each do |at|
+          # if current user is the lender
+          time_now_update = Time.now
+          if at.lender_id == current_user.id
+            # check for duplication
+            if !active_trans_ids.include? at.borrower_id
+              lsa = User.find(at.borrower_id).profile.last_seen_at
+              # p 'Last seen ' + User.find(at.borrower_id).full_name + ' at ' + lsa.to_s
+              # p 'Time now is  ' + time_now_update.to_s
+              # p ' Difference is 1 ' + (time_now_update - lsa).seconds.to_s
+              if !lsa.nil? && (time_now_update - lsa).seconds < 6.5
+                active_trans_ids.push(at.borrower_id)
+              end
+            end
+          else
+            # current user is the borrower
+            # check for duplication
+            if !active_trans_ids.include? at.lender_id
+              # p 'Last seen ' + User.find(at.lender_id).full_name + ' at ' + lsa.to_s
+              # p 'Time now is  ' + time_now_update.now.to_s
+              # p ' Difference is 2 ' + (time_now_update.now - lsa).seconds.to_s
+              if !lsa.nil? && (Time.now - lsa).seconds < 6.5
+                active_trans_ids.push(at.lender_id)
+              end
+            end
+          end
+        end
+        # For home page / search page, transaction in progress need to be listed
+        # Returns transaction ids. So need need for duplication checks
+      else
+        # puts ' Checking for ' + User.find(current_user.id).full_name + ' - ' + current_user.id.to_s
+        #TODO Check if chatbox call is cheaper than the quesries themselves
+        chatbox
+        @current_transactions.each do |ct|
+          # current user is the lender
+          time_now_update = Time.now
+          if ct.lender_id == current_user.id
+            lsa = User.find(ct.borrower_id).profile.last_seen_at
+            # p 'Last seen ' + User.find(ct.borrower_id).full_name + ' at ' + lsa.to_s
             # p 'Time now is  ' + time_now_update.to_s
-            # p ' Difference is 1 ' + (time_now_update - lsa).seconds.to_s
+            # p ' Difference is 3 ' + (time_now_update - lsa).seconds.to_s
+            if !lsa.nil? && (time_now_update - lsa).seconds < 6
+              active_trans_ids.push(ct.id)
+            end
+          else
+            # current user is the borrower
+            lsa = User.find(ct.lender_id).profile.last_seen_at.to_time
+            # p 'Last seen ' + User.find(ct.lender_id).full_name + ' at ' + lsa.to_s
+            # p 'Time now is  ' + time_now_update.to_s
+            # p ' Difference is 4 ' + (time_now_update - lsa).seconds.to_s
             if !lsa.nil? && (time_now_update - lsa).seconds < 6.5
-              active_trans_ids.push(at.borrower_id)
-            end
-          end
-        else
-          # current user is the borrower
-          # check for duplication
-          if !active_trans_ids.include? at.lender_id
-            # p 'Last seen ' + User.find(at.lender_id).full_name + ' at ' + lsa.to_s
-            # p 'Time now is  ' + time_now_update.now.to_s
-            # p ' Difference is 2 ' + (time_now_update.now - lsa).seconds.to_s
-            if !lsa.nil? && (Time.now - lsa).seconds < 6.5
-              active_trans_ids.push(at.lender_id)
+              active_trans_ids.push(ct.id)
             end
           end
         end
       end
-      # For home page / search page, transaction in progress need to be listed
-      # Returns transaction ids. So need need for duplication checks
-    else
-      # puts ' Checking for ' + User.find(current_user.id).full_name + ' - ' + current_user.id.to_s
-      #TODO Check if chatbox call is cheaper than the quesries themselves
-      chatbox
-      @current_transactions.each do |ct|
-        # current user is the lender
-        time_now_update = Time.now
-        if ct.lender_id == current_user.id
-          lsa = User.find(ct.borrower_id).profile.last_seen_at
-          # p 'Last seen ' + User.find(ct.borrower_id).full_name + ' at ' + lsa.to_s
-          # p 'Time now is  ' + time_now_update.to_s
-          # p ' Difference is 3 ' + (time_now_update - lsa).seconds.to_s
-          if !lsa.nil? && (time_now_update - lsa).seconds < 6
-            active_trans_ids.push(ct.id)
-          end
-        else
-          # current user is the borrower
-          lsa = User.find(ct.lender_id).profile.last_seen_at.to_time
-          # p 'Last seen ' + User.find(ct.lender_id).full_name + ' at ' + lsa.to_s
-          # p 'Time now is  ' + time_now_update.to_s
-          # p ' Difference is 4 ' + (time_now_update - lsa).seconds.to_s
-          if !lsa.nil? && (time_now_update - lsa).seconds < 6.5
-            active_trans_ids.push(ct.id)
-          end
-        end
-      end
-    end
 
-    respond_to do |format|
-      # p ' Sending list to caller ' + active_trans_ids.inspect
-      format.json { render json: active_trans_ids.to_json }
+      respond_to do |format|
+        # p ' Sending list to caller ' + active_trans_ids.inspect
+        format.json { render json: active_trans_ids.to_json }
+      end
     end
   end
 
