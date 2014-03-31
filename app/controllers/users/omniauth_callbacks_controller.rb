0b9f920ad266c4a8397136740f70ea34bb996547
Siddharth Bhagwan <sid_sss@hotmail.com>
2014-03-31 17:54:00 +0530

    Styling, Devise Changes. Code Cleaned

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user=User.from_omniauth(request.env['omniauth.auth'])
 
     if user.persisted?
-      flash.notice = 'Signed In!'
+      flash.notice = 'Signed In!' if user.confirmed?
       sign_in_and_redirect user
     else
       session['devise.user_attributes'] = user.attributes
