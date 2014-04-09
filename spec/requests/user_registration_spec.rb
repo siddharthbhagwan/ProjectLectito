266bc00055952f176987c34cc33e4f03581b1b4c
Siddharth Bhagwan <sid_sss@hotmail.com>
2014-04-09 09:43:52 +0530

    Devise new user registration rspec added

---
+++ spec/requests/user_registration_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'user registration' do
+  it 'allows new users to register with an email address and password' do
+    visit '/users/sign_up'
+
+    fill_in 'Email',                 with: 'sidunderscoresss@gmail.com',  match: :first
+    fill_in 'Password',              with: 'password', match: :first
+    fill_in 'Confirm Password',      with: 'password'
+
+    click_button 'Sign up'
+
+    page.should have_content('A message with a confirmation link has been sent to your email address. Please open the link to activate your account ')
+  end
+end
