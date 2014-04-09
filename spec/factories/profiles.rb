2fe4c35e9f39eb98a0403f9e99234140507a1738
Siddharth Bhagwan <sid_sss@hotmail.com>
2014-04-09 09:32:02 +0530

    Profile Factory removed

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,9 +1,5 @@
 FactoryGirl.define do 
   factory :profile do |f|
-    user_first_name 'Siddhartha'
-    user_last_name 'Bhagwan'
-    user_phone_no '9637396836'
-    DoB: Date.today
-    gender 'M'
+
   end
 end
